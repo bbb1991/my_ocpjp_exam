87cdd1b5983793ccc689fd8602292dbc171022c2
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2016-01-11 02:34:30 +0600

    Add some code

---
+++ src/main/java/DummyIdeas/JsonParserExample.java
@@ -0,0 +1,7 @@
+package DummyIdeas;
+
+/**
+ * Created by bbb1991 on 1/11/16.
+ */
+public class JsonParserExample {
+}
---
+++ src/main/java/advanced_class_design/enumExample1.java
@@ -0,0 +1,7 @@
+package advanced_class_design;
+
+/**
+ * Created by bbb1991 on 1/11/16.
+ */
+public class enumExample1 {
+}
